6d4c4b08ce681faf83db04a1b433521e08f1e2a7
foza <francisco.zuniga@gft.com>
2017-08-21 06:13:43 -0600

    added dev tools for redux

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,7 +1,10 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from  'redux-devtools-extension';
 import reducers from '../reducers';
 
 export default function configStore() {
-  return createStore(reducers, {}, applyMiddleware(reduxThunk));
+  return createStore(reducers, {}, composeWithDevTools(
+    applyMiddleware(reduxThunk)
+  ));
 }
