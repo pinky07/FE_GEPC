f4236aa59692a439339ba4d241cbd0b25fb9a4f9
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-09-28 08:42:25 -0600

    Changed App.js import to app.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configStore from './store/configStore';
 import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
+import App from './app';
 import registerServiceWorker from './registerServiceWorker';
 
 const store = configStore();
