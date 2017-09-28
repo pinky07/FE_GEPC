41be654b13bca3eef9b49d30facf914c956d0e36
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-09-28 10:51:40 -0600

    Header now matches new wireframaes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import allocationTree from './TreeReducer';
 import allocationGrid from './GridReducer';
 import shared from './sharedReducer';
+import spotFire from './spotFireReducer'
 
 export default combineReducers({
   allocationTree,
   allocationGrid,
-  shared
+  shared,
+  spotFire,
 });
