8154e4ca8d05e845bde67d1c4293e129c2b03653
Rubén Jiménez Goñi <ruben.jimenez@gft.com>
2017-09-28 11:32:38 -0600

    Added Redux Logger

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger'
 import reduxThunk from 'redux-thunk';
-import { composeWithDevTools } from  'redux-devtools-extension';
+
 import reducers from '../reducers';
 
 export default function configStore() {
   return createStore(reducers, {}, composeWithDevTools(
-    applyMiddleware(reduxThunk)
+    applyMiddleware(
+      logger,
+      reduxThunk,
+    )
   ));
 }
